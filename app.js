b3fbb8fbc64e0b1d1d8f01433b4e86ba6516cdb2
Timofeev Dmitrii <goshanoob@mail.ru>
2020-05-27 20:26:49 +0500

    Первая версия

---
+++ app.js
@@ -0,0 +1,46 @@
+const status = document.getElementById('status');
+const messages = document.getElementById('messages');
+const input = document.getElementById('input');
+
+const ws = new WebSocket('ws://localhost:2999');
+for(let i=0, tasks = document.getElementsByClassName('tasks'), len = tasks.length; i < len; i++){
+    tasks[i].addEventListener('click', choose);
+}
+function choose(){
+    ws.send(JSON.stringify({username: 'goshanoob', choosenTask: this.id}));
+    
+    for(let i=0, tasks = document.getElementsByClassName('tasks'), len = tasks.length; i < len; i++){
+        tasks[i].removeEventListener('click', choose);
+    }
+}
+
+function setStatus(value){
+    status.innerHTML = value;
+}
+
+function printMessage(value){
+    for(let i=0; i<value.choosen.length; i++){
+        document.getElementById(value.choosen[i]).className = 'tasks choosen';
+    }
+    
+    if(value.hasOwnProperty("username")){
+        const div = document.createElement('div');
+        div.innerHTML = `Пользователь ${value.username} дропнул билет № ${value.choosenTask.replace(/\D/g,"")}. Удачи`;
+        messages.appendChild(div);
+    }
+    
+}
+
+
+/*
+document.getElementById("input").addEventListener('keydown', event =>{
+    if(event.keyCode === 13){
+        ws.send(JSON.stringify({username: 'goshan_loh',message: input.value}));
+        input.value = "";
+    }
+});*/
+
+
+ws.onopen = () => setStatus('Подключился');
+ws.onclose =() => setStatus('Отключился');
+ws.onmessage = response => printMessage(JSON.parse(response.data));
---
+++ server.js
@@ -0,0 +1,88 @@
+const WebSocket = require('ws');
+const server  = new WebSocket.Server({port: 2999});
+var choosen = [];
+server.on('connection', ws => {
+  server.clients.forEach(client => {
+    if(client.readyState === WebSocket.OPEN){
+        client.send(JSON.stringify({choosen:choosen}));
+    }
+  });
+    ws.on('message', data => {
+        
+        data = JSON.parse(data);
+        choosen.push(data.choosenTask);
+        data.choosen = choosen;
+        server.clients.forEach(client => {
+            if(client.readyState === WebSocket.OPEN){
+                client.send(JSON.stringify(data));
+            }
+        });
+        
+        let date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        storeInMySQL(data.username, date, data.choosenTask.replace(/\D/g,''));
+    })
+    ws.send('Сервер арбайтен');
+});
+
+// заненсение сообщений в БД
+function storeInMySQL(user,date,mess){
+  const mysql = require("mysql2");
+  const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'goshanoob',
+      database: 'chat',
+      password: 'P3cJk33A0q'
+  });
+
+  const sql = `INSERT INTO messages(user,date,message) VALUES('${user}','${date}','${mess}')`;
+  
+  connection.query(sql, function(err, results) {
+      if(err) console.log(err);
+      console.log(results);
+  });
+  
+  connection.end();
+}
+
+// поиск данных в БД
+function searchInMySQL(task){
+  const mysql = require("mysql2");
+  const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'goshanoob',
+      database: 'tasks',
+      password: 'P3cJk33A0q'
+  });
+
+  const sql = `INSERT INTO messages(user,date,message) VALUES('${user}','${date}','${mess}')`;
+  
+  connection.query(sql, function(err, results) {
+      if(err) console.log(err);
+      console.log(results);
+
+
+      clients[id] = ws
+
+
+
+  });
+  
+  connection.end();
+}
+
+/*
+connection.connect(function(err){
+    if (err) {
+      return console.error("Ошибка: " + err.message);
+    }
+    else{
+      console.log("Подключение к серверу MySQL успешно установлено");
+    }
+ });
+
+ connection.end(function(err) {
+    if (err) {
+      return console.log("Ошибка: " + err.message);
+    }
+    console.log("Подключение закрыто");
+  });*/
