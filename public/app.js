35609a6ce4e1e15a007a3003076769bb42b9f9ea
Timofeev Dmitrii <goshanoob@mail.ru>
2020-06-04 19:17:57 +0500

    Сделал идентификацию, поиск

--- public/app.js
+++ public/app.js
@@ -1,39 +1,112 @@
-const status = document.getElementById('status');
 const messages = document.getElementById('messages');
-const input = document.getElementById('input');
+var FIO = '';
 
-
-var HOST = location.origin.replace(/^http/, 'ws')
+const HOST = location.origin.replace(/^http/, 'ws')
 const ws = new WebSocket(HOST);
 
-for(let i=0, tasks = document.getElementsByClassName('tasks'), len = tasks.length; i < len; i++){
-    tasks[i].addEventListener('click', choose);
-}
-function choose(){
-    ws.send(JSON.stringify({username: 'goshanoob', choosenTask: this.id}));
-    
-    for(let i=0, tasks = document.getElementsByClassName('tasks'), len = tasks.length; i < len; i++){
-        tasks[i].removeEventListener('click', choose);
+ws.onopen = () => setStatus('Клиент: Подключен, ');
+ws.onclose =() => setStatus('Клиент: Отключился, ');
+ws.onmessage = response => requestEvent(JSON.parse(response.data));
+
+/* Логика срабатывания на сообщения сервера */
+function requestEvent(value){
+    switch(value.messType){
+        case 'time': document.getElementById('time').innerHTML = `Время сервера: ${value.message}`; break;
+        case 'status': document.getElementById('statusServ').innerHTML = value.message; break;
+        case 'findUser': showUsers(value.message); break;
+        case 'identification': makeIdentification(value); break;
+        case 'message': addMessage(value.message); break;
+        case 'choose': setChoose(value); break;
     }
 }
 
-function setStatus(value){
-    status.innerHTML = value;
+/* Функция для вывода сообщений о клиенте*/
+function setStatus(value, param){
+    document.getElementById('status').innerHTML = value;
 }
 
-function printMessage(value){
-    for(let i=0; i<value.choosen.length; i++){
-        document.getElementById(value.choosen[i]).className = 'tasks choosen';
+/* Поиск пользователя при вводе */ 
+document.getElementById('userName').addEventListener('input', e => {
+    const str = e.target.value;
+    if(str.length > 0){
+        ws.send(JSON.stringify({messType: 'findUser', username: str}));
     }
-    
+});
+
+/* Функция показа найденных пользователей */
+function showUsers(users){
+    const res = document.getElementById('searchResults');
+    res.innerHTML = "";
+    res.style.display = "block";
+    users.forEach(name => {
+        const div = document.createElement('div'); 
+        div.innerHTML = name; 
+        div.className = 'res';
+        res.appendChild(div);
+        div.addEventListener('click', e => {
+            document.getElementById('userName').value = e.target.innerHTML;
+            e.target.parentNode.style.display = 'none';
+        });
+    });
+}
+// скрыть найденных пользователей при щелчке в любом месте документа
+document.addEventListener('click', () => {
+    document.getElementById('searchResults').style.display = 'none'
+});
+
+/* Подтверждение выбора пользователя */
+document.getElementById('sendName').addEventListener('click', () => {
+    ws.send(JSON.stringify({messType: 'identification', username: document.getElementById('userName').value}));
+});
+
+/* Сервер обработал запрос на идентификацию и прислал ответ */
+function makeIdentification(value){
+    document.getElementById('statusIdent').innerHTML = value.message;
+    // если сервер прислал имя пользователя, то пользователь найден
     if(value.hasOwnProperty("username")){
-        const div = document.createElement('div');
-        div.innerHTML = `Пользователь ${value.username} дропнул билет № ${value.choosenTask.replace(/\D/g,"")}. Удачи`;
-        messages.appendChild(div);
+        // делаем билеты доступными для выбора
+        makeTask(); 
+        FIO = value.username;
     }
-    
 }
 
+/* Вывод сообщений от сервера */
+function addMessage(value){
+    const div = document.createElement('div');
+    div.innerHTML = value;
+    messages.appendChild(div);
+}
+
+/* Регистрация слушателей на каждый билет */
+function makeTask(){
+    for(let i=0, tasks = document.getElementsByClassName('tasks'), len = tasks.length; i < len; i++){
+        tasks[i].addEventListener('click', makeChoose);
+    }
+}
+
+/* Отправка номера билета и удаление слушателей */
+function makeChoose(){
+    if(FIO === '' || FIO === 'undefined'){
+        alert('Вы не прошли идентификацию');
+        return;
+    }
+    ws.send(JSON.stringify({messType: 'choose', username: FIO, choosenTask: this.id}));
+    for(let i=0, tasks = document.getElementsByClassName('tasks'), len = tasks.length; i < len; i++){
+        tasks[i].removeEventListener('click', makeChoose);
+    }
+}
+
+function setChoose(value){
+    /* Пометить выбранные билеты стилями */
+    for(let i=0, len = value.choosenTask.length; i < len; i++){
+        document.getElementById(value.choosenTask[i]).className = 'tasks choosen';
+    }
+    
+    /* Если появились новые выбранные билеты, то информируем сообщением от сервера  */
+    if(value.hasOwnProperty("task")){
+        addMessage(`Пользователь ${value.username} дропнул билет № ${value.task.replace(/\D/g,"")}. Удачи`);
+    }
+}
 
 /*
 document.getElementById("input").addEventListener('keydown', event =>{
@@ -44,6 +117,4 @@ document.getElementById("input").addEventListener('keydown', event =>{
 });*/
 
 
-ws.onopen = () => setStatus('Подключился');
-ws.onclose =() => setStatus('Отключился');
-ws.onmessage = response => printMessage(JSON.parse(response.data));
+
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 
-
 var port = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
@@ -12,29 +11,148 @@ use(express.static(__dirname + '/public'))
 const { Server } = require('ws');
 const wss = new Server({ server });
 
+// выбранные билеты
 var choosen = [];
+// номера задач
+const nums = [10, 8, 7, 1, 4, 5, 2, 9, 6, 3];
+// перечень пользователей
+var users = {
+  'Бояршинова Александра Сергеевна': 0,
+  'Габбасов Радмир Рамилевич': 0,
+  'Гоффоров Нурбекжон Обиджон угли': 0,
+  'Гребенщиков Александр Александрович': 0,
+  'Деньгина Анастасия Андреевна': 0,
+  'Закирова Юлиана Раилевна': 0,
+  'Кугушев Даниил Ильич': 0,
+  'Мухаметов Рустам Данилович': 0,
+  'Николаев Николай Андреевич': 0,
+  'Потураев Глеб Сергеевич': 0,
+  'Пушняков Василий Сергеевич': 0,
+  'Рахматуллин Серафим Маратович': 0,
+  'Хомяков Илья Андреевич': 0,
+  'Шашуро Максим Владимирович': 0,
+  'Шердакова Екатерина Сергеевна': 0,
+  'Ярушин Михаил Валерьевич': 0,
+  'username': 0
+}
+
 wss.on('connection', ws => {
+  /* Клиенту уходит массив выбранных билетов */
   wss.clients.forEach(client => {
-
-        client.send(JSON.stringify({choosen:choosen}));
-    
+        client.send(JSON.stringify({messType:'choose', choosenTask:choosen}));
   });
-  ws.on('message', data => {
-        
-        data = JSON.parse(data);
-        choosen.push(data.choosenTask);
-        data.choosen = choosen;
-        wss.clients.forEach(client => {
 
-                client.send(JSON.stringify(data));
+  /* На сервер пришло сообщение ... */
+  ws.on('message', data => {
+        data = JSON.parse(data);
+        /* Определяем тип сообщения */
+        switch(data.messType){
+          case 'findUser': findUser2(); break;
+          case 'identification': getId(); break;
+          case 'choose': getChoose(); break;
+        }
+
+        // Поиск пользователя в БД - только десять подключений в heroku, лучше сохраню в массиве, всего 17 стедунтов
+        function findUser(){
+          let connect = connectMySQL();
+          const sql = "SELECT * FROM users WHERE name LIKE ? ORDER BY name";
+          connect.query(sql, data.username+'%', (err, results) => {
+            if(err) console.log(err);
+            console.log(results);
+          });
+          connect.end();
+        }
+
+        /* Поиск пользователя по списку */
+        function findUser2(){
+          let reg = new RegExp('^'+data.username, 'i'), resp =[];
+          /*for(let key in users){
+            if(key.match(reg)){
+              resp.push(key);
+            }
+          }
+          console.log(resp);*/
+          let result = Object.keys(users).filter(user => user.match(reg));
+          console.log(result);
+          ws.send(JSON.stringify({messType:'findUser', message:result}));
+        }
+
+        function getId(){
+          if(users.hasOwnProperty(data.username)){
+            let username = data.username;
+            // Прислать номер билета, если уже выбран, либо оповестить о входе
+            if(users[username] !== 0){
+              ws.send(JSON.stringify({messType:'identification', message:`Вы уже выбрали билет №${users[username]}`}));
+            } else {
+              ws.send(JSON.stringify({messType:'identification', message:'Вы вошли - уже на троечку (шутка)', 
+                username: username})
+              );
+              wss.clients.forEach(client => {
+                client.send(JSON.stringify({messType: 'message', message:`${username} вошел (-а). Ура!`}));
+              });
+            }
             
-        });
-        
-        let date = new Date().toISOString().slice(0, 19).replace('T', ' ');
-        //storeInMySQL(data.username, date, data.choosenTask.replace(/\D/g,''));
+          } else {
+            ws.send(JSON.stringify({messType:'identification', message:'Пользователь не найден. Проверьте имя!'}));
+          }
+        }
+
+        /* Обработка выбора пользователя */
+        function getChoose(){
+          /*
+            Просмотреть выбранные билеты, убедиться, что билет не был выбран
+            кем-то ранее, и прислать подтверждение клиенту, либо сообщить о неудаче
+          */
+          let username = data.username;
+          let task = data.choosenTask;
+          let num = nums[task.replace(/\D/g,'')];
+          let date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+          let wasChoose = false; // для контроля повторного билета
+          choosen.forEach(c => {
+            if(c === task){
+              return wasChoose = true;
+            }
+          });
+          if(wasChoose){
+            ws.send(JSON.stringify({messType:'identification', 
+              message:'К сожалению, этот билет уже кто-то выбрал. Сделайте другой выбор...',
+              username: username})
+            );
+            return;
+          }
+          console.log(username, users[username]);
+          if(users.hasOwnProperty(username) && users[username] === 0){
+            users[username] = task;
+            choosen.push(task);
+            ws.send(JSON.stringify({messType:'identification', message:`Вы дропнули задачу №${num}`}));
+            wss.clients.forEach(client => {
+              client.send(JSON.stringify({messType:'choose', username: username, choosenTask:choosen, task: task}));
+            });
+            //storeInMySQL(username, date, task.replace(/\D/g,''));
+            console.log(`${username} выбрал ${task} в ${date}`);
+          } else {
+            ws.send(JSON.stringify({messType:'identification', message:"Странно..."}));
+          }
+        }
     });
-    ws.send('Сервер арбайтен');
+
+    ws.send(JSON.stringify({messType: 'status', message: 'Сервер арбайтен'}));
+    
+    setInterval( () => {
+      ws.send(JSON.stringify({messType: 'time', message: new Date().toISOString().slice(0, 19).replace('T', ' ')}));
+    }, 10000);
 });
+
+
+// подключение в БД
+function connectMySQL(){
+  return require("mysql2").createConnection({
+      host: 'localhost',
+      user: 'goshanoob',
+      database: 'chat',
+      password: 'P3cJk33A0q'
+  });
+}
 
 // заненсение сообщений в БД
 /*
